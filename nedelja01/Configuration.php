85e35d158af5cd0391c0b25b8c12c7e19e47e49e
Nikola Ristovski <singidunum.nikola.ristovski@gmail.com>
2019-05-28 20:25:38 +0200

    nedelja 14

--- nedelja01/Configuration.php
+++ nedelja01/Configuration.php
@@ -1,11 +1,11 @@
 <?php
     final class Configuration {
-        const BASE = 'http://localhost/nedelja01/';
+        const BASE = 'http://localhost/KatalogLaptopova/KatalogLaptopova/nedelja01/';
 
         const DATABASE_HOST = 'localhost';
-        const DATABASE_USER = 'student';
-        const DATABASE_PASS = 'student';
-        const DATABASE_NAME = 'auction_project';
+        const DATABASE_USER = 'root';
+        const DATABASE_PASS = '';
+        const DATABASE_NAME = 'bazaKatalogLaptopova';
 
         const SESSION_STORAGE_CLASS = '\\App\\Core\\Session\\FileSessionStorage';
         const SESSION_STORAGE_ARGUMENTS = [ './session/' ]; # !!!
